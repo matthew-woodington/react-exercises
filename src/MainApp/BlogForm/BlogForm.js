fa416ce9b58293e4d508f0784e041552040b762d
matthew-woodington <smwoodington@gmail.com>
2022-09-28 14:51:08 -0400

    adds solution for app a

--- src/MainApp/BlogForm/BlogForm.js
+++ src/MainApp/BlogForm/BlogForm.js
@@ -1,5 +1,41 @@
+import { useState } from 'react';
+import { nanoid } from "nanoid";
+
 function BlogForm() {
-  return <div>I am a BlogForm</div>;
+
+  const [blogs, setBlogs] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBlog = {
+      title,
+      body,
+      id: nanoid(),
+    };
+    setBlogs([...blogs, newBlog]);
+    clearForm();
+  }
+
+  const clearForm = () => {
+    setTitle('');
+    setBody('');
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Blog Title:</label>
+        <input name='title' id="title-text" type="text" placeholder='Blog title here...' value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <label htmlFor="body">Content:</label>
+        <textarea name="body" id="body-text" cols="30" rows="10" placeholder='Enter blog content here...' value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <button type='button' onClick={(e) => clearForm()}>Clear</button>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default BlogForm;
